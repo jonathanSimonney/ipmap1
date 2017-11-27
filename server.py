6fcfdeef102b81a7e9260961581d1098213035d3
Jonathan SIMONNEY <jonathan.simonney@supinternet.fr>
2017-11-27 11:20:03 +0100

    functional, but no cache is done.

---
+++ server.py
@@ -0,0 +1,57 @@
+# !/usr/bin/env python
+
+# multiprocessing, threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+import argparse
+import asyncio
+import sys
+from functools import lru_cache
+from aiohttp import web
+if sys.platform == 'win32':
+    loop = asyncio.ProactorEventLoop()
+    asyncio.set_event_loop(loop)
+
+
+async def run_command(*args):
+    # Create subprocess
+    process = await asyncio.create_subprocess_exec(
+        *args,
+        # stdout must a pipe to be accessible as process.stdout
+        stdout=asyncio.subprocess.PIPE)
+    # Wait for the subprocess to finish
+    stdout, stderr = await process.communicate()
+    # Return stdout
+    return stdout.decode().strip()
+
+
+async def handle(request):
+    ip = request.match_info.get('ip', "")
+    if ip == "":
+        text = "Hello"
+    else:
+        # Gather uname and date commands
+
+        commands = run_command('nmap', '-sV', ip)
+
+        result = await commands
+
+        text = str(format(result))
+    return web.Response(text=text)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port")
+
+    parser.add_argument("--host")
+    args = parser.parse_args()
+
+    print(int(args.port), args.host)
+    app = web.Application()
+    app.router.add_get('/{ip}', handle)
+
+    web.run_app(app, host=args.host, port=int(args.port))
+
+
+main()
